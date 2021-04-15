a1dcdd6128aa96efa84c29ad76d4979cc43791dc
HelinBulut <helinbulut92@gmail.com>
2021-04-16 00:00:22 +0300

    Proje

---
+++ src/test/java/Pages/LoginPage.java
@@ -0,0 +1,34 @@
+package Pages;
+
+import Contants.LoginPageContants;
+import base.BasePage;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+
+
+public class LoginPage extends BasePage implements LoginPageContants {
+
+    private Logger LOGGER = LogManager.getLogger(LoginPage.class);
+    String email="hunerfurkan";
+    String sifre="Furkan11";
+
+    public void goToLoginPage (){
+        waitSeconds(1);
+        String actualTitle;
+        actualTitle = driver.getTitle();
+        Assert.assertTrue("anasayfada değilsiniz", actualTitle.equals("Üye Giriş Sayfası – Gittigidiyor.com"));
+
+    }
+
+    public void Login(){
+        waitForElementAndClick(login);
+        waitForElementAndClick(loginbutton);
+        waitForElementAndSendKeys(emailText,email);
+        waitForElementAndSendKeys(sifreText,sifre);
+        waitForElementAndClick(girisYap);
+        Assert.assertTrue("Hesabım elementi gözükmedi. Login işlemi başarısız.",isElementVisible(submitKontrol,10));
+        LOGGER.info("Başarılı şekılde gırış yapıldı.");
+    }
+
+}
